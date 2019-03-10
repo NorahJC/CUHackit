449b9870595f943b1553970d970a0354ea544b16
NorahJC <norahjeanc@gmail.com>
2019-03-10 04:34:42 -0400

    Added ex files of the Alexa program

---
+++ Alexa_QuestionGame_questions.js
@@ -0,0 +1,28 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_US: [
+    {
+      'Which of the following is not a stable sorting algorithm in its typical implementation.': [
+        'Quick sort',
+        'Merge sort',
+        'Insertion sort', 
+        'Bubble sort',
+        /*quick sort*/
+      ],
+    },
+    {
+      'The maximum number of binary trees that can be formed with three unlabeled nodes is: ': [
+        '5',
+        '1',
+        '4',
+        '3',
+       /*5*/
+      ],
+    },
+  ],
+};
